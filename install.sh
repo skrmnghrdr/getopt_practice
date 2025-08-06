38f38b32c0eeccfa29be86d7da79a28014901980
Dimasilang, Smith <notpaulogrian@protonmail.com>
2025-08-05 22:29:02 -0400

    fixed install.sh
    added the build essential

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 mkdir build
 touch build/build.sh
 sudo apt-get update 
+sudo apt update && sudo apt install build-essential -y
 sudo apt-get install cmake -y
 
 echo "cmake --build . --target clean && cmake --build ." > build/build.sh
